3f09ad7dd0f2be003772deb5be2dbf1757063e0e
codehath <codingfarhath@gmail.com>
2024-12-16 23:13:48 +0000

    Added inline svg to dashboard for dark mode - top buttons aren't centered on mobile - needs to be fixed

--- src/routes/day/+page.svelte
+++ src/routes/day/+page.svelte
@@ -78,11 +78,30 @@
 
 {#if user}
   <div class="dashboard-container module-colour">
-    <img
+    <!-- Use this to centre if inline doesnt work -->
+    <!-- <img
       class="dashboard-image"
       src="/images/module-dashboard-shape.svg"
       alt="dashboard-shape"
-    />
+    /> -->
+    <svg class="dashboard-shape" width="1086" height="330" viewBox="0 0 1086 330" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_d_418_36)">
+        <path d="M15 31C15 19.9543 23.9543 11 35 11H1051C1062.05 11 1071 19.9543 1071 31V291C1071 302.046 1062.05 311 1051 311H35C23.9543 311 15 302.046 15 291V31Z" fill="var(--logout-color)"/>
+        <path d="M35 11.5H1051C1061.77 11.5 1070.5 20.2304 1070.5 31V291C1070.5 301.77 1061.77 310.5 1051 310.5H35C24.2304 310.5 15.5 301.77 15.5 291V31C15.5 20.2304 24.2304 11.5 35 11.5Z" stroke="var(--border-color)"/>
+      </g>
+      <defs>
+        <filter id="filter0_d_418_36" x="0.2" y="0.2" width="1085.6" height="329.6" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="7.4"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_418_36"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_418_36" result="shape"/>
+        </filter>
+      </defs>
+    </svg>
     <div class="dashboard-contents">
       <a class="home-button" href="/dashboard"
         ><img src="/images/home-circle-button.svg" alt="home button" /></a
@@ -155,6 +174,11 @@
 {/if}
 
 <style>
+  .dashboard-shape{
+    width: 100%;
+    height: auto;
+    max-height: 307px;
+  }
   .activity {
     margin: 5%;
     position: relative;
